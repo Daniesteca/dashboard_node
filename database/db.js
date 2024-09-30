6d359c66803eb02a26d12f06bd5d8737e1764624
Daniela <nanysmanila@hotmail.com>
2024-09-30 07:33:53 -0500

    actualizo conexion

--- database/db.js
+++ database/db.js
@@ -3,7 +3,7 @@ const mysql = require('mysql2');
 const conexion = mysql.createConnection({
     host: 'localhost',
     user: 'root',
-    password: '',
+    password: 'Toor963.',
     database:'charts'
 })
 
