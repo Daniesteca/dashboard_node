eb49c62da9edd7276476db8e232f936c9a1fd983
Daniela <nanysmanila@hotmail.com>
2024-09-30 14:13:43 -0500

    modifico db

--- database/db.js
+++ database/db.js
@@ -6,22 +6,6 @@ const pool = mysql.createPool({
     password: 'Toor*963.',
     database:'charts'
 })
-// const conexion = mysql.createConnection({
-//     host: 'localhost',
-//     user: 'root',
-//     password: 'Toor*963.',
-//     database:'charts'
-// })
-// conexion.connect((error)=>{
-//     if(error){
-//         console.error('El error de conexion es : '+error);
-//         return
-//     }
-//     console.log('¡Conectado a la BD Mysql!');
-// })
-
-// module.exports = conexion;
-
 // Confirmar la conexión
 pool.getConnection((error, connection) => {
     if (error) {
